83300041dc51be52116b5122b276a7092d83ef4f
Nguyễn Hữu Nghĩa <nhnske@gmail.com>
2020-08-15 23:19:08 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ import sqlite3
 
 wd = get_webdriver()
 category_id = 77 # thoi trang nu
-items = get_items(wd, ['vay'], 1, category_id)
+items = get_items(wd, ['vay', 'quan', 'ao', 'phu kien'], 5, category_id)
 item_id_0, shop_id_0 = next(iter(items))
 print(item_id_0, shop_id_0)
 print(items[(item_id_0, shop_id_0)].keys())
